316935b431ceaa3f5129b49391927897c8a99ec9
Avinash <avinashrana55@yahoo.com>
2025-10-09 18:24:30 +0530

    Created Product and shipping info model

---
+++ server/models/productTable.js
@@ -0,0 +1,26 @@
+import database from '../database/db';
+
+export async function createProductTable(){
+    try {
+        const query = `
+            CREATE TABLE IF NOT EXISTS products (
+                id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                description TEXT NOT NULL,
+                price DECIMAL(5, 2) NOT NULL CHECK(price >= 0),
+                category VARCHAR(100) NOT NULL,
+                ratings decimal(3, 2) DEFAULT 0 CHECK (ratings BETWEEN 0 AND 5),
+                images JSONB DEFAULT '[]'::JSONB,
+                stock INT NOT NULL CHECK (stock >= 0),
+                created_by UUID NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,'
+                FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE CASCADE
+            );
+        `;
+        await database.query(query);
+
+    } catch (error) {
+        console.error("Failed to Create Products Table.", error);
+        process.exit(1);
+    }
+}
