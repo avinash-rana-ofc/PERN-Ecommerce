1f66b6f7cb4fcc4565c1e84b2e52e3b219d15bfc
Avinash <avinashrana55@yahoo.com>
2025-10-14 18:22:35 +0530

    Created JWToken

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,10 +1,12 @@
 import ErrorHandler from "../middlewares/errorMiddleware.js";
-import { catchAsyncErrors } from "../middlewares/catchAsyncError";
+import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
 import database from "../database/db.js";
 import bcrypt from "bcrypt";
+import { sendToken } from "../utils/jwtToken.js";
 
 export const register = catchAsyncErrors(async(req, res, next) => {
     const {name, email, password} = req.body;
+
     if(!name || !email || !password){
         return next(new ErrorHandler("Please provide all required fields.", 400));
     }
@@ -17,7 +19,17 @@ export const register = catchAsyncErrors(async(req, res, next) => {
         return next(new ErrorHandler("Email already registered.", 400));
     }
 
-    const hashedPassword = await bcrypt.ha
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await database.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`,
+         [name, email, hashedPassword]
+    );
+
+    sendToken(user.rows[0], 201, "User registered successfully", res);
+
+
+
+
 });
 export const login = catchAsyncErrors(async(req, res, next) => {});
 export const getUser = catchAsyncErrors(async(req, res, next) => {});
