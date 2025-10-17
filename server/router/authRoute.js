81e93f5afae654ec29c7bf31fd14518765ac5d0f
Avinash <avinashrana55@yahoo.com>
2025-10-17 17:50:35 +0530

    Created updated profile

--- server/router/authRoute.js
+++ server/router/authRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { forgotPassword, getUser, login, logout, register, resetPassword, updatePassword } from "../controllers/authController.js";
+import { forgotPassword, getUser, login, logout, register, resetPassword, updatePassword, updateProfile } from "../controllers/authController.js";
 import { isAuthenticated } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -10,6 +10,7 @@ router.get("/logout", isAuthenticated, logout);
 router.get("/me", isAuthenticated, getUser);
 router.post("/password/forgot", forgotPassword);
 router.put("/password/reset/:token", resetPassword);
-router.put("/password/update", isAuthenticated, updatePassword)
+router.put("/password/update", isAuthenticated, updatePassword);
+router.put("/profile/update", isAuthenticated, updateProfile);
 
 export default router;
