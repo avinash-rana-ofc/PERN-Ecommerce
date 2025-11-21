b2b1df3e6e3703a57f588549f7a1646ec6edcf85
Avinash <avinashrana55@yahoo.com>
2025-11-21 18:40:53 +0530

    Added Stripe payment Integration

--- server/app.js
+++ server/app.js
@@ -2,27 +2,92 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
-import {errorMiddleware} from "./middlewares/errorMiddleware.js";
-import authRouter  from "./router/authRoute.js";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+import authRouter from "./router/authRoute.js";
 import productRouter from "./router/productRoute.js";
 import adminRouter from "./router/adminRouter.js";
 import { createTables } from "./utils/createTables.js";
+import Stripe from "stripe";
+import { database } from "./database/db.js";
 const app = express();
 
-app.use(cors({
-    origin : [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
-    methods : ["GET", "POST", "PUT", "DELETE"],
-    credentials : true
-}));
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
+//=================STRIPE INTEGRATION=======================
+app.post(
+  "/api/v1/payment/webhook",
+  express.raw({ type: "application/json" }),
+  async (req, res) => {
+    const sig = req.headers["stripe-signature"];
+    let event;
+    try {
+      event = Stripe.webhooks.constructEvent(
+        req.body,
+        sig,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (error) {
+      return res
+        .status(400)
+        .send(`Webhook Error : ${error.message} || ${error}`);
+    }
+
+    //Handling Event
+    if (event.type === "payment_intent.succeeded") {
+      const paymentIntent_client_secret = event.data.object.client_secret;
+
+      try {
+        //FINDING UPDATE PAYMENT
+        const updatePaymentStatus = "Paid";
+        const paymentTableUpdateResult = await database.query(
+          `UPDATE payments SET payment_status = $1 WHERE payment_intent_id = $2 RETURNING *`,
+          [updatePaymentStatus, paymentIntent_client_secret]
+        );
+        const orderTableUpdateResult = await database.query(
+          `UPDATE orders SET paid_at = NOW() WHERE id = $1 RETURNING *`,
+          [paymentTableUpdateResult.rows[0].order_id]
+        );
+
+        //REDUCE STOCK FOR EACH PRODUCT
+        const orderId = paymentTableUpdateResult.rows[0].order_id;
+        const { rows: orderedItems } = await database.query(
+          `SELECT product_id, quantity FROM order_items WHERE order_id = $1`,
+          [orderId]
+        );
+
+        //FOR EACH ORDERED ITEM, REDUCE THE PRODUCT STOCK
+        for (const item of orderedItems) {
+          await database.query(
+            `UPDATE products SET stock = stock - $1 WHERE id = $2`,
+            [item.quantity, item.product_id]
+          );
+        }
+      } catch (error) {
+        return res
+          .status(500)
+          .send(`Error updating paid_at timestamp in orders table`);
+      }
+    }
+    res.status(200).send({ received: true });
+  }
+);
+//==========================END=============================
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(fileUpload({
-    tempFileDir : "./uploads",
-    useTempFiles : true
-}))
+app.use(
+  fileUpload({
+    tempFileDir: "./uploads",
+    useTempFiles: true,
+  })
+);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/product", productRouter);
